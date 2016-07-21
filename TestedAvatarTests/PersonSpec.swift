b7c50952d782e3f8d4c9f60ca42491ec4280f6e1
Ben Norris <code@bsn.design>
2016-07-21 08:59:52 -0600

    Add test stubs

--- TestedAvatarTests/PersonSpec.swift
+++ TestedAvatarTests/PersonSpec.swift
@@ -11,4 +11,34 @@ import XCTest
 
 class PersonSpec: XCTestCase {
     
+    /// test that it initializes correctly with no parameters
+    func testThatItInitializesCorrectlyWithNoParameters() {
+        
+    }
+    
+    /// test that it initializes correctly with only first name
+    func testThatItInitializesCorrectlyWithOnlyFirstName() {
+        
+    }
+    
+    /// test that it initializes correctly with only last name
+    func testThatItInitializesCorrectlyWithOnlyLastName() {
+        
+    }
+    
+    /// test that it initializes correctly with first and last name
+    func testThatItInitializesCorrectlyWithFirstAndLastName() {
+        
+    }
+    
+    /// test that it conforms to name presentable correctly
+    func testThatItConformsToNamePresentableCorrectly() {
+        
+    }
+    
+    /// test that it conforms to avatar presentable correctly
+    func testThatItConformsToAvatarPresentableCorrectly() {
+        
+    }
+    
 }
