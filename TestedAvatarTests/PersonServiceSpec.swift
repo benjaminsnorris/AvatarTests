a87862a3fb567a72ed883a7e866e399a8575bf5a
Ben Norris <code@bsn.design>
2016-07-21 09:13:12 -0600

    Start to add tests for person service

--- TestedAvatarTests/PersonServiceSpec.swift
+++ TestedAvatarTests/PersonServiceSpec.swift
@@ -11,4 +11,30 @@ import XCTest
 
 class PersonServiceSpec: XCTestCase {
     
+    var service: PersonService!
+    
+    override func setUp() {
+        super.setUp()
+        service = PersonService()
+    }
+    
+    /// test that current person is nil when initialized
+    func testThatCurrentPersonIsNilWhenInitialized() {
+        XCTAssertNil(PersonService.currentPerson)
+    }
+    
+    /// test that current person is not nil after first name is saved
+    func testThatCurrentPersonIsNotNilAfterFirstNameIsSaved() {
+        XCTAssertNil(PersonService.currentPerson)
+        service.saveFirstName("Sir")
+        XCTAssertNotNil(PersonService.currentPerson)
+    }
+    
+    /// test that current person is not nil after last name is saved
+    func testThatCurrentPersonIsNotNilAfterLastNameIsSaved() {
+        XCTAssertNil(PersonService.currentPerson)
+        service.saveLastName("Lancelot")
+        XCTAssertNotNil(PersonService.currentPerson)
+    }
+
 }
