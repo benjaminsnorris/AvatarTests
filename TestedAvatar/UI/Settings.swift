b34030252c22d4ee7d6978cac53fb420c02811fb
Ben Norris <code@bsn.design>
2016-07-21 07:32:50 -0600

    Add first and last name fields in Settings

--- TestedAvatar/UI/Settings.swift
+++ TestedAvatar/UI/Settings.swift
@@ -10,4 +10,21 @@ import UIKit
 
 class Settings: UIViewController {
     
+    // MARK: - IB properties
+    
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    
+}
+
+
+// MARK: - Text field delegate
+
+extension Settings: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
